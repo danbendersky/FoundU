acff3cc7531f896c4a432dfad74ef740fb2dc821
Sodiumtelluride <gelfandnat@gmail.com>
2024-11-15 17:07:25 -0500

    Made Add Item Form
    Co-Authored-By: Alan Achilles <122038420+alanNEF@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Button from '@mui/material/Button'
-
+import FormPage from './pages/form/FormPage.jsx'
 function App() {
   return (
     <>
-      <Button variant="text">Text</Button>
+      <FormPage />
     </>
   )
 }
