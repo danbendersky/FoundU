958d80967ddd16b5e1eaeebd573aad668457ad19
danbendersky <dbendersky@umass.edu>
2024-11-17 13:28:57 -0500

    update packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Button from '@mui/material/Button'
 import Nav from './components/Nav/Nav.jsx'
 import FormPage from './pages/FormPage/FormPage.jsx'
 function App() {
