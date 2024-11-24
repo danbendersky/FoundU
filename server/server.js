7cbb2ffa42621cdcacdc593f4c1302c3f6d511da
danbendersky <dbendersky@umass.edu>
2024-11-24 16:11:45 -0500

    Created route and model

--- server/server.js
+++ server/server.js
@@ -7,11 +7,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
 // Connect to MongoDB
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {});
 const connection = mongoose.connection;
-// Define routes and middleware
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
+
+//Define routes
+const userRouter = require('./routes/users');
+app.use('/users', userRouter);
+
+const eventRouter = require('./routes/events');
+app.use('/events', eventRouter);
+
+// Start listen
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
