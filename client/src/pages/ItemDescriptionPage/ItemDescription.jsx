a50f5b5bb6f946aee0f89ce2e50528f6442d3d70
danbendersky <dbendersky@umass.edu>
2024-11-24 16:34:33 -0500

    Added logo to ItemPage

--- client/src/pages/ItemDescriptionPage/ItemDescription.jsx
+++ client/src/pages/ItemDescriptionPage/ItemDescription.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../../index.css'
 import ItemPage from '../../components/ItemDescription/ItemDescription';
+import Nav from '../../components/Nav/Nav.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Nav />
     <ItemPage />
   </StrictMode>,
 )
