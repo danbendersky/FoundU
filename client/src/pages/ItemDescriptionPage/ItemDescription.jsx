dc50f47ea3889a0efa07c516543cd8482ef3057c
danbendersky <dbendersky@umass.edu>
2024-11-24 16:23:25 -0500

    Fixed item description page (component issue)

--- client/src/pages/ItemDescriptionPage/ItemDescription.jsx
+++ client/src/pages/ItemDescriptionPage/ItemDescription.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../../index.css'
+import ItemPage from '../../components/ItemDescription/ItemDescription';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
