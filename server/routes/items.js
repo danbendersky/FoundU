c5612ea3a6d346ecafa1e8d0c7ba9167a16d8464
Kushaan <kushaannaskar@gmail.com>
2025-01-04 22:05:20 -0800

    Update items.js
    fixed typo in get method

--- server/routes/items.js
+++ server/routes/items.js
@@ -15,7 +15,7 @@ router.post('/postItem', async (req, res) => {
 });
 
 //Get all items
-router.get('items', async(req, res) =>{
+router.get('/items', async(req, res) =>{
     try{
         const items = await Item.find();
         res.status(200).json(items);
