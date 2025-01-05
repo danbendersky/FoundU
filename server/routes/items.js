e4b29d3fa6d9c8f8352edbd06f5bc2828f4a1d24
Kushaan <kushaannaskar@gmail.com>
2025-01-04 21:51:34 -0800

    Update items.js
    Fixed missing parenthesis on get items

--- server/routes/items.js
+++ server/routes/items.js
@@ -15,17 +15,20 @@ router.post('/postItem', async (req, res) => {
 });
 
 //Get all items
-router.get('items', async(req, res) =>){
+router.get('items', async(req, res) =>{
     try{
         const items = await Item.find();
         res.status(200).json(items);
     } catch(error){
         res.status(500).json({ error: 'Failed to get items'});
     }
-}
+});
 
 
 //Patch item to be claimed
+router.patch('claimItem/:itemId', async(req, res) =>{
+
+})
 
 //Put item when item is updated (only by user who created it)
 
